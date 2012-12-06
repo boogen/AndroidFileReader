f92caa178bb5c0703ca1fe42181e5e2186a614a7
Marcin Bugala <marcin.bugala@cantstopgames.com>
2012-12-06 14:21:10 +0100

    utils

--- build.sh
+++ build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-acompc -source-path ./DefaultLib/src/ -include-classes com.cantstopgames.filereader.FileReader com.cantstopgames.filereader.ImageLoader com.cantstopgames.filereader.XmlLoader com.cantstopgames.filereader.RawDataLoader -swf-version=14 -output build/default/FileReaderAPI.swc
+acompc -source-path ./DefaultLib/src/ -include-classes com.cantstopgames.utils.Utils com.cantstopgames.utils.ImageLoader com.cantstopgames.utils.XmlLoader com.cantstopgames.utils.RawDataLoader -swf-version=14 -output build/default/UtilsAPI.swc
  
-unzip -o build/default/FileReaderAPI.swc -d build/default
+unzip -o build/default/UtilsAPI.swc -d build/default
 rm build/default/catalog.xml
-rm build/default/FileReaderAPI.swc
+rm build/default/UtilsAPI.swc
 
-acompc -source-path ./AndroidSwcLib/src/ -include-classes com.cantstopgames.filereader.FileReader com.cantstopgames.filereader.ImageLoader com.cantstopgames.filereader.XmlLoader com.cantstopgames.filereader.RawDataLoader -swf-version=14 -output build/Android-ARM/FileReaderAPI.swc
+acompc -source-path ./AndroidSwcLib/src/ -include-classes com.cantstopgames.utils.Utils com.cantstopgames.utils.ImageLoader com.cantstopgames.utils.XmlLoader com.cantstopgames.utils.RawDataLoader -swf-version=14 -output build/Android-ARM/UtilsAPI.swc
 
-unzip -o build/Android-ARM/FileReaderAPI.swc -d build/Android-ARM
+unzip -o build/Android-ARM/UtilsAPI.swc -d build/Android-ARM
 rm build/Android-ARM/catalog.xml
-mv build/Android-ARM/FileReaderAPI.swc build/
+mv build/Android-ARM/UtilsAPI.swc build/
 
 cd build
 
-adt.bat -package -target ane com.cantstopgames.FileReader.ane extension.xml -swc FileReaderAPI.swc -platform Android-ARM -C Android-ARM ReadFileAPI.jar library.swf -platform default -C default/ .
+adt.bat -package -target ane com.cantstopgames.utils.ane extension.xml -swc UtilsAPI.swc -platform Android-ARM -C Android-ARM UtilsAPI.jar library.swf -platform default -C default/ .
 
